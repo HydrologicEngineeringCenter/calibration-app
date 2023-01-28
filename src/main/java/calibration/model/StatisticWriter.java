aad415053668398d3bd2ae89b27fc5eecd3e6a91
Brennan Beam <BrennanBeam@gmail.com>
2023-01-27 16:35:42 -0800

    bug fixes StatisticWriter

--- src/main/java/calibration/model/StatisticWriter.java
+++ src/main/java/calibration/model/StatisticWriter.java
@@ -1,8 +1,6 @@
 package calibration.model;
-
 import calibration.statistics.Statistic;
 import javafx.collections.ObservableList;
-
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -11,51 +9,46 @@ public class StatisticWriter {
     FileWriter ratingWriter;
     boolean valueHeadersWritten = false;
     boolean ratingHeadersWritten = false;
-
     public StatisticWriter(String valuesOutputFile, String moriasiRatingOutputFile) {
         try{
             this.valueWriter = new FileWriter(valuesOutputFile);
             this.ratingWriter = new FileWriter(moriasiRatingOutputFile);
         }
         catch (Exception ex){
-            System.out.println(ex);
+            ex.printStackTrace();
         }
     }
-
     public void write(String name, ObservableList<Statistic> stats) throws IOException {
         writeStatisticsValues(name,stats);
         writeStatisticsRatings(name,stats);
     }
     private void writeStatisticsValues(String name, ObservableList<Statistic> stats) throws IOException {
-        if(valueHeadersWritten){
+        if(!valueHeadersWritten){
             WriteHeader(valueWriter,stats);
-            ratingHeadersWritten = true;
+            valueHeadersWritten = true;
         }
         valueWriter.write("\n" + name + ",");
-        for(int i = 0; i<stats.size();i++){
-            valueWriter.write(stats.get(i).getValue() +",");
+        for (Statistic stat : stats) {
+            valueWriter.write(stat.getValue() + ",");
         }
+        valueWriter.flush();
     }
-
     private void writeStatisticsRatings(String name, ObservableList<Statistic> stats) throws IOException {
-        if(ratingHeadersWritten){
+        if(!ratingHeadersWritten){
             WriteHeader(ratingWriter,stats);
             ratingHeadersWritten = true;
         }
         ratingWriter.write("\n" + name + ",");
-        for(int i = 0; i<stats.size();i++){
-            ratingWriter.write(stats.get(i).getRating() +",");
+        for (Statistic stat : stats) {
+            ratingWriter.write(stat.getRating() + ",");
         }
+        ratingWriter.flush();
     }
-
     private void WriteHeader(FileWriter writer, ObservableList<Statistic> stats) throws IOException {
-        if(valueHeadersWritten){
-            WriteHeader(ratingWriter,stats);
-            ratingHeadersWritten = false;
-        }
         writer.write("Name,");
-        for(int i = 0; i<stats.size();i++){
-            ratingWriter.write(stats.get(i).getName() +",");
+        for (Statistic stat : stats) {
+            writer.write(stat.getName() + ",");
         }
+        writer.flush();
     }
 }
