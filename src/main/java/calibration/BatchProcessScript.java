f02526b790ddd232ab2063a2fef6f7e284841bb3
Brennan Beam <BrennanBeam@gmail.com>
2023-01-27 16:35:21 -0800

    refine batch process script

--- src/main/java/calibration/BatchProcessScript.java
+++ src/main/java/calibration/BatchProcessScript.java
@@ -11,18 +11,22 @@ import java.io.IOException;
 public class BatchProcessScript {
     public static void main(String[] args) throws IOException {
         //String directoryPath = args[0];
-        String directoryPath = "D:\\FFRD\\CalibrationTester\\";
+        String directoryPath = "D:\\FFRD\\CalibrationAppStudy\\";
         File directory = new File(directoryPath);
+        File[] files = directory.listFiles();
 
-        StatisticWriter writer = new StatisticWriter(directoryPath + "calibrationsValues.txt", directoryPath + "calibrationsRatings.txt");
+        StatisticWriter writer = new StatisticWriter(directoryPath + "calibrationsValues.csv",
+                directoryPath + "calibrationsRatings.csv");
+        for (File file : files) {
+            if(file.getAbsolutePath().endsWith(".xml")){
+                Analysis analysis = new Analysis(file);
+                analysis.validate();
+                analysis.process();
+                String name = file.getName();
+                ObservableList<Statistic> stats = analysis.getStatistics();
+                writer.write(name, stats);
+            }
 
-        for (File file : directory.listFiles()) {
-            Analysis analysis = new Analysis(file);
-            analysis.validate();
-            analysis.process();
-            String name = file.getName();
-            ObservableList<Statistic> stats = analysis.getStatistics();
-            writer.write(name, stats);
         }
 
     }
